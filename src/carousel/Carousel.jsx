7041da909c7eb19d3929f6edfc58e645c100ebf0
Petya Angelova <bedfordpetya@gmail.com>
2022-08-28 12:09:58 +0300

    change slides

--- src/carousel/Carousel.jsx
+++ src/carousel/Carousel.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import CarouselItem from "./CarouselItem";
 import "./Carousel.css"
 
 const Carousel = ({slides}) => {
+    const [currentSlide, setCurrentSlide] = useState(0)
+
+    useEffect(() => {
+        const slideInterval = setInterval(() => {
+            setCurrentSlide(currentSlide => currentSlide < slides.length -1 ? currentSlide + 1 : 0)
+        }, 3000);
+
+        return () => clearInterval(slideInterval)
+
+    }, [])
+
     return (
         <div className="carousel">
-            <div className="carousel-inner">
+            <div 
+                className="carousel-inner" 
+                style={{ transform: `translateX(${-currentSlide * 100}%)`}}
+            >
             {slides.map((slide, index) => (
                 <CarouselItem slide={slide} key={index} />
             ))}
